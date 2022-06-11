67dc951645ba3c7847301c02d675082af29ce4bf
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2022-06-11 23:26:31 +0600

    fixing

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+<<<<<<< HEAD
+=======
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+>>>>>>> parent of ca0584b... add product stocks crud
 
 class Product extends Model
 {
